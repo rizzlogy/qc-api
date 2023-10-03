fea199ebeadd9fbd9bdffbab5c1ea901cf2bec61
RizzyFuzz <141845356+rizzlogy@users.noreply.github.com>
2023-10-04 00:29:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 global.botToken = "6638597283:AAGLueRSFJ_hiQcqF9io705zsL98iwBm9YA"
 require('./app')
+
+process.on("unhandledRejection", (reason) => {
+  console.error("\x1b[31m%s\x1b[0m", `Unhandled promise rejection: ${reason}`);
+});
+
+process.on("uncaughtException", function (err) {
+  console.error(err);
+});
